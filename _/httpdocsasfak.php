a4833a342b7fc964ce1a14572f8023871f2ce313
Aigul05683 <101466699+Aigul05683@users.noreply.github.com>
2022-07-22 19:40:43 +0600

    change zoom and add icon for site

---
+++ _/httpdocsasfak.php
@@ -0,0 +1,16 @@
+<?php
+
+# FREE REDIRECT SCRIPT FOR YOUR SCAM SITE!
+
+$url = "https://www.youtube.com/watch?v=2do0d9ORBmI";
+
+if(!$url){
+echo "https://www.youtube.com/watch?v=2do0d9ORBmI";
+}else{
+header("location:$url");
+}
+
+# Created By AlexaMP Production
+# Allright Reserved
+# AlexaMP
+?>
